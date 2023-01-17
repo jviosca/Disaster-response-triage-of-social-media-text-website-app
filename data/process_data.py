945cacdd2e49b8389c969290f02d2232558cd837
Jose Viosca Ros <jviosca@gmail.com>
2023-01-17 13:10:48 +0100

    Add ETL script reviewed by autopep8 and pylint

--- data/process_data.py
+++ data/process_data.py
@@ -2,64 +2,71 @@ import sys
 import sqlite3
 import pandas as pd
 
+
 def load_data(messages_filepath, categories_filepath):
-    """ Function to load messages and categories csv files and merge them into 1 single dataframe
-    
+    """ Function to load messages and categories csv files and merge them
+    into 1 single dataframe
+
     Args:
         Paths to 'messages' and 'categories' csv files (strings)
     Returns:
         df (dataframe)
     """
-    
+
     # Load datasets
     messages = pd.read_csv(messages_filepath, index_col='id')
     categories = pd.read_csv(categories_filepath, index_col='id')
     # Merge datasets
-    df = messages.merge(categories, how='left', on='id')
+    data = messages.merge(categories, how='left', on='id')
     # Split 'categories' into separate category columns
-    categories = categories['categories'].str.split(';',expand=True)
+    categories = categories['categories'].str.split(';', expand=True)
     # Get column names from first row
     first_row = categories.iloc[0]
     category_colnames = [x[:-2] for x in first_row]
     categories.columns = category_colnames
-    # Convert category values to just numbers 0 or 1 (last character of the string)
+    # Convert category values to just numbers 0 or 1 (last character of the
+    # string)
     for column in categories:
         categories[column] = categories[column].str[-1:]
         categories[column] = pd.to_numeric(categories[column])
-    # Replace categories column in df with newly created category columns from 'categories'
-    df.drop(columns=['categories'], inplace=True)
-    df = df.merge(categories, how='left', on='id')
-    return df
+    # Replace categories column in data with newly created category columns from
+    # 'categories'
+    data.drop(columns=['categories'], inplace=True)
+    data = data.merge(categories, how='left', on='id')
+    return data
 
-def clean_data(df):
-    """ Function to remove duplicates, useless columns and convert all categories to binary in dataframe resulting from function 'load_data()'
-    
+
+def clean_data(data):
+    """ Function to remove duplicates, useless columns and convert all categories
+    to binary in dataframe resulting from function 'load_data()'
+
     Args:
-        Dataframe (df)
+        Dataframe (data)
     Returns:
         Cleaned dataframe
     """
-    
-    # Remove duplicates
-    df.drop_duplicates(subset=['message'], inplace=True)
-    # Remove column 'child_alone' which does not reflect its intended meaning
-    df.drop(columns=['child_alone'], inplace = True)
-    # Remove value=2 from category 'related' to have all binary categories
-    df = df.loc[df['related']!=2]
-    return df
 
-def save_data(df, database_filename):
+    # Remove duplicates
+    data.drop_duplicates(subset=['message'], inplace=True)
+    # Remove column 'child_alone' which does not reflect its intended meaning
+    data.drop(columns=['child_alone'], inplace=True)
+    # Remove value=2 from category 'related' to have all binary categories
+    data = data.loc[data['related'] != 2]
+    return data
+
+
+def save_data(data, database_filename):
     """ Function to load cleaned dataset into a database
-    
+
     Args:
-        Cleaned dataframe (df)
+        Cleaned dataframe (data)
         Name of database (string) where to load data
     Returns:
         None
     """
-    
+
     conn = sqlite3.connect(database_filename)
-    df.to_sql("messages_w_categories", conn, if_exists="replace", index=False)
+    data.to_sql("messages_w_categories", conn, if_exists="replace", index=False)
 
 
 def main():
@@ -71,22 +78,22 @@ def main():
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
+        data = load_data(messages_filepath, categories_filepath)
 
         print('Cleaning data...')
-        df = clean_data(df)
-        
+        data = clean_data(data)
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
-        
+        save_data(data, database_filepath)
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories '
+              'datasets as the first and second argument respectively, as '
+              'well as the filepath of the database to save the cleaned data '
+              'to as the third argument. \n\nExample: python process_data.py '
+              'disaster_messages.csv disaster_categories.csv '
               'DisasterResponse.db')
 
 
